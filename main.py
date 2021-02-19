45e2e71ec6dba95af8e3ea2e152bcc7517945036
45 <newtankman2@mail.ru>
2021-02-19 16:31:18 +0300

    еуые

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-import pytest
-from selenium import webdriver
-
-link = "http://selenium1py.pythonanywhere.com/"
+import unittest
 
 
-@pytest.fixture
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    # этот код выполнится после завершения теста
-    print("\nquit browser..")
-    browser.quit()
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+
+    def test_abs2(self):
+        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
 
 
-class TestMainPage1():
-    # вызываем фикстуру в тесте, передав ее как параметр
-    def test_guest_should_see_login_link(self, browser):
-        browser.get(link)
-        browser.find_element_by_css_selector("#login_link")
-
-    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-        browser.get(link)
-        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+if __name__ == "__main__":
+    unittest.main()
