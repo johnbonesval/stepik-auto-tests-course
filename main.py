582a54296148f7ff2594bbd16d3e6ebe1b69b47e
45 <newtankman2@mail.ru>
2021-02-19 15:35:21 +0300

    fixture

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
-import math
-import os
 from selenium import webdriver
-import time
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 
-try:
-    link = "http://suninjuly.github.io/redirect_accept.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
+class TestMainPage1():
 
-    # Ваш код, который заполняет обязательные поля
-    x = 2
+    @classmethod
+    def setup_class(self):
+        print("\nstart browser for test suite..")
+        self.browser = webdriver.Chrome()
 
+    @classmethod
+    def teardown_class(self):
+        print("quit browser for test suite..")
+        self.browser.quit()
 
+    def test_guest_should_see_login_link(self):
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector("#login_link")
 
-    # Отправляем заполненную форму
-    button = browser.find_element_by_css_selector("button.btn")
-    button.click()
-
-    new_window = browser.window_handles[1]
-    first_window = browser.window_handles[0]
-    browser.switch_to.window(new_window)
-
-    x1_element = browser.find_element_by_id("input_value")
-    x1 = x1_element.text
-    y = str(math.log(abs(12 * math.sin(int(x1)))))
-    input3 = browser.find_element_by_id("answer")
-    input3.send_keys(y)
-
-    button = browser.find_element_by_css_selector("button.btn")
-    button.click()
-
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
 
 
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(3)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+class TestMainPage2():
+
+    def setup_method(self):
+        print("start browser for test..")
+        self.browser = webdriver.Chrome()
+
+    def teardown_method(self):
+        print("quit browser for test..")
+        self.browser.quit()
+
+    def test_guest_should_see_login_link(self):
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector("#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
