8a16666e7e2d7b0f65fc166d4b24b9d36a6f3dec
45 <newtankman2@mail.ru>
2021-02-19 15:02:38 +0300

    ff

---
+++ main.py
@@ -0,0 +1,42 @@
+import math
+import os
+from selenium import webdriver
+import time
+
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+
+
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    new_window = browser.window_handles[1]
+    first_window = browser.window_handles[0]
+    browser.switch_to.window(new_window)
+
+    x1_element = browser.find_element_by_id("input_value")
+    x1 = x1_element.text
+    y = str(math.log(abs(12 * math.sin(int(x1)))))
+    input3 = browser.find_element_by_id("answer")
+    input3.send_keys(y)
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(3)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
